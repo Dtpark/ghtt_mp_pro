4df8eeb3674d6181c3b4e7787465503826dd4fe3
Dtpark <dtpark@ghtt.net>
2020-07-20 18:30:41 +0800

    1.一卡通箭头大小调整；2.课表箭头样式&颜色调整；3.添加快讯滚动效果；4.确定课表课程名称字号

--- pages/home/home/home.js
+++ pages/home/home/home.js
@@ -1,145 +1,158 @@
 // pages/home/home/home.js
 Page({
-  options: {
-    addGlobalClass: true,
-  },
+    options: {
+        addGlobalClass: true,
+    },
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    bannerImgUrls: {
-      0:{
-        image_url:
-          "https://ghttdata.hitwh.cc/data/attachment/forum/201907/08/151218k9m51mm5f61mk5v9.png",
-        type: 0,
-        title: '',
-        d_id: ''
-      },
-      1:{
-        image_url:
-          "https://ghttdata.hitwh.cc/data/attachment/forum/201907/08/151218k9m51mm5f61mk5v9.png",
-        type: 0,
-        title: '',
-        d_id: ''
-      }
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        // 轮播图
+        bannerImgUrls: {
+            0: {
+                image_url: "https://ghttdata.hitwh.cc/data/attachment/forum/201907/08/151218k9m51mm5f61mk5v9.png",
+                type: 0,
+                title: '',
+                d_id: ''
+            },
+            1: {
+                image_url: "https://ghttdata.hitwh.cc/data/attachment/forum/201907/08/151218k9m51mm5f61mk5v9.png",
+                type: 0,
+                title: '',
+                d_id: ''
+            }
+
+        },
+
+        // 快讯（公告）列表
+        noticeList: {
+            0: {
+                type: '通知',
+                title: '关于巴拉巴拉小魔仙的123456789012345'
+            },
+            1: {
+                type: '通知',
+                title: '关于巴拉巴拉小魔仙的123456789012345'
+            }
+        },
+
+
+        // 应用列表
+        iconList: [{
+            icon: 'cardboardfill',
+            color: 'red',
+            badge: 120,
+            name: 'VR'
+        }, {
+            icon: 'recordfill',
+            color: 'orange',
+            badge: 1,
+            name: '录像'
+        }, {
+            icon: 'picfill',
+            color: 'yellow',
+            badge: 0,
+            name: '图像'
+        }, {
+            icon: 'noticefill',
+            color: 'olive',
+            badge: 22,
+            name: '通知'
+        }, {
+            icon: 'upstagefill',
+            color: 'cyan',
+            badge: 0,
+            name: '排行榜'
+        }, {
+            icon: 'clothesfill',
+            color: 'blue',
+            badge: 0,
+            name: '皮肤'
+        }, {
+            icon: 'discoverfill',
+            color: 'purple',
+            badge: 0,
+            name: '发现'
+        }, {
+            icon: 'questionfill',
+            color: 'mauve',
+            badge: 0,
+            name: '帮助'
+        }, {
+            icon: 'commandfill',
+            color: 'purple',
+            badge: 0,
+            name: '问答'
+        }, {
+            icon: 'brandfill',
+            color: 'mauve',
+            badge: 0,
+            name: '版权'
+        }],
+        // 课表列表
+        todayCurriculum: {
+            0: {
+                name: '11111',
+                time: '111122',
+                room: '33333'
+            }
+        }
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
 
     },
-    // 应用列表
-    iconList: [{
-      icon: 'cardboardfill',
-      color: 'red',
-      badge: 120,
-      name: 'VR'
-    }, {
-      icon: 'recordfill',
-      color: 'orange',
-      badge: 1,
-      name: '录像'
-    }, {
-      icon: 'picfill',
-      color: 'yellow',
-      badge: 0,
-      name: '图像'
-    }, {
-      icon: 'noticefill',
-      color: 'olive',
-      badge: 22,
-      name: '通知'
-    }, {
-      icon: 'upstagefill',
-      color: 'cyan',
-      badge: 0,
-      name: '排行榜'
-    }, {
-      icon: 'clothesfill',
-      color: 'blue',
-      badge: 0,
-      name: '皮肤'
-    }, {
-      icon: 'discoverfill',
-      color: 'purple',
-      badge: 0,
-      name: '发现'
-    }, {
-      icon: 'questionfill',
-      color: 'mauve',
-      badge: 0,
-      name: '帮助'
-    }, {
-      icon: 'commandfill',
-      color: 'purple',
-      badge: 0,
-      name: '问答'
-    }, {
-      icon: 'brandfill',
-      color: 'mauve',
-      badge: 0,
-      name: '版权'
-    }],
-    // 课表列表
-    todayCurriculum:{
-      0:{
-        name: '11111',
-        time: '111122',
-        room: '33333'
-      }
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
+        console.log(1234)
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
+
     }
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    console.log(1234)
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
