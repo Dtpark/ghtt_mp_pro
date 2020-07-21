23d5545728f2b9e97812471d3d23ef113d23d7ad
Dtpark <dtpark@ghtt.net>
2020-07-21 20:01:47 +0800

    发现页完成帖子列表的加载和附件图片的显示

--- pages/find/home/home.js
+++ pages/find/home/home.js
@@ -1,5 +1,10 @@
 // pages/find/home/home.js
 const app = getApp()
+const newrepUrl = require('../../../config/config').newrepUrl
+const newestUrl = require('../../../config/config').newestUrl
+const hotUrl = require('../../../config/config').hotUrl
+const digestUrl = require('../../../config/config').digestUrl
+const userAvatar = require('../../../config/config').userAvatar
 Component({
     options: {
         addGlobalClass: true,
@@ -29,31 +34,54 @@ Component({
         // 分类导航栏是否固定
         fix: false,
         // 顶部导航列表
-        navList: {
-            0: {
+        navList: [{
                 title: "最新回复"
             },
-            1: {
+            {
                 title: "最新主题"
             },
-            2: {
+            {
                 title: "热门主题"
             },
-            3: {
+            {
                 title: "精华主题"
             },
-        },
+        ],
         // 索引
         index: 0,
         TabCur: 0,
-        scrollLeft: 0
+        scrollLeft: 0,
+
+        // 帖子列表
+        datalist: [],
+        // 帖子页面
+        page: 1,
+
+        userAvatar: userAvatar
+    },
+
+    /**
+     * 组件的生命周期
+     */
+    lifetimes: {
+        attached: function() {
+            // 在组件实例进入页面节点树时执行
+            let that = this
+
+            // 获取帖子列表
+            that.getListData()
+
+
+        },
+        detached: function() {
+            // 在组件实例被从页面节点树移除时执行
+        }
     },
 
     /**
      * 组件的方法列表
      */
     methods: {
-
         // 控制导航栏固定与否
         bindScroll(e) {
             let that = this
@@ -69,14 +97,99 @@ Component({
             }
         },
 
-
-
         // 切换顶部导航页面
         tabSelect(e) {
-            this.setData({
+            let that = this
+            that.setData({
                 TabCur: e.currentTarget.dataset.id,
-                scrollLeft: (e.currentTarget.dataset.id - 1) * 60
+                scrollLeft: (e.currentTarget.dataset.id - 1) * 60,
+                page: 1
             })
-        }
+            that.getListData()
+
+        },
+
+        // 请求列表数据(最新回复/最新主题/热门回复/精华主题)
+        getListData() {
+            let that = this
+            let url = ''
+            let tabCur = that.data.TabCur
+                // 选择主题类型
+            switch (tabCur) {
+                case 0:
+                    url = newrepUrl
+                    break;
+                case 1:
+                    url = newestUrl
+                    break
+                case 2:
+                    url = hotUrl
+                    break
+                case 3:
+                    url = digestUrl
+                    break
+                default:
+                    url = newrepUrl
+            }
+
+            // 要请求第几页
+            let data = {
+                page: that.data.page,
+            }
+            app.apimanager.getRequest(url, data).then(res => {
+                app.wxApi.hideLoading();
+                // wx.stopPullDownRefresh()
+                // 请求到的帖子列表
+                let datalist = res.Variables.data ? res.Variables.data : []
+                for (let i = 0; i < datalist.length; i++) {
+                    let postItem = datalist[i]
+                    let listindex = i + that.data.datalist.length
+                        // ================附件处理 ===================
+                    let attachlist = postItem.attachlist
+                    let imageA = []
+                    let audioA = []
+                    let videoA = []
+                    let DownloadA = [];
+                    for (let aidKey in attachlist) {
+                        let attItem = attachlist[aidKey]
+                        let newUrl = attItem.attachment
+                        attItem['newUrl'] = newUrl
+                        if (attItem.type == 'image') {
+                            imageA.push(attItem)
+                        } else if (attItem.type == 'audio') {
+                            let total_process = '00:00'
+                            if (attItem.description) {
+                                total_process = util.formatTime(parseInt(attItem.description))
+                            }
+                            attItem['toolUse'] = {
+                                    attachment: newUrl,
+                                    listIndex: listindex,
+                                    total_process: total_process
+                                }
+                                // console.log(attItem);
+                            audioA.push(attItem)
+                        } else if (attItem.type == 'video') {
+                            videoA.push(attItem)
+                        } else if (attItem.ext == 'pdf' || attItem.ext == 'ppt' || attItem.ext == 'pptx' || attItem.ext == 'docx' || attItem.ext == 'doc' || attItem.ext == 'xls' || attItem.ext == 'xlsx') {
+                            DownloadA.push(attItem);
+                        }
+                    }
+                    postItem['imageA'] = imageA
+                    postItem['audioA'] = audioA
+                    postItem['videoA'] = videoA
+                    postItem['DownloadA'] = DownloadA;
+                }
+                if (that.data.page > 1) {
+                    datalist = that.data.datalist.concat(datalist)
+                }
+                that.setData({
+                    datalist: datalist
+                })
+            }).catch(res => {
+                app.wxApi.hideLoading();
+                // wx.stopPullDownRefresh();
+            })
+        },
+
     }
 })
--- pages/mine/home/home.js
+++ pages/mine/home/home.js
@@ -4,160 +4,160 @@ const loginPath = require('../../../utils/path').default.loginPath
 const settingPath = require('../../../utils/path').default.settingPath
 const userAvatar = require('../../../config/config').userAvatar
 Component({
-  options: {
-    addGlobalClass: true,
-  },
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-
-  },
-
-  /**
-   * 组件的声明周期
-   */
-  lifetimes: {
-    attached: function () {
-      // 在组件实例进入页面节点树时执行
-      let that = this
-      // 判断登录态
-      let uid = app.globalData.uid
-      let token = app.globalData.token
-      if (uid == '' || token == '') {
-        // 本地没有登录信息
-        that.setData({
-          avatar: userAvatar,
-          username: '',
-          uid: '',
-          threads: '-',
-          posts: '-',
-          credits: '-',
-          field4: '',
-          grouptitle: ''
-        })
-      } else {
-        // 本地存在登录信息
-        // 设置用户id
-        that.setData({
-          uid: uid
-        })
-        // 获取dz的用户信息
-        that.requestProfile()
-      }
-      
+    options: {
+        addGlobalClass: true,
     },
-    detached: function () {
-      // 在组件实例被从页面节点树移除时执行
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+
+    },
+
+    /**
+     * 组件的生命周期
+     */
+    lifetimes: {
+        attached: function() {
+            // 在组件实例进入页面节点树时执行
+            let that = this
+                // 判断登录态
+            let uid = app.globalData.uid
+            let token = app.globalData.token
+            if (uid == '' || token == '') {
+                // 本地没有登录信息
+                that.setData({
+                    avatar: userAvatar,
+                    username: '',
+                    uid: '',
+                    threads: '-',
+                    posts: '-',
+                    credits: '-',
+                    field4: '',
+                    grouptitle: ''
+                })
+            } else {
+                // 本地存在登录信息
+                // 设置用户id
+                that.setData({
+                        uid: uid
+                    })
+                    // 获取dz的用户信息
+                that.requestProfile()
+            }
+
+        },
+        detached: function() {
+            // 在组件实例被从页面节点树移除时执行
+        }
+    },
+
+    /**
+     * 组件所在页面的生命周期
+     */
+    pageLifetimes: {
+        show: function() {
+            // 页面被展示
+            let that = this
+                // 判断登录态
+            let uid = app.globalData.uid
+            let token = app.globalData.token
+            if (uid == '' || token == '') {
+                // 本地没有登录信息
+                that.setData({
+                    avatar: userAvatar,
+                    username: '',
+                    uid: '',
+                    threads: '-',
+                    posts: '-',
+                    credits: '-',
+                    field4: '',
+                    grouptitle: ''
+                })
+            } else {
+                // 本地存在登录信息
+                // 设置用户id
+                that.setData({
+                        uid: uid
+                    })
+                    // 获取dz的用户信息
+                that.requestProfile()
+            }
+        },
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        // 头像链接
+        avatar: userAvatar,
+        // 用户名（昵称）
+        username: '',
+        // 用户id
+        uid: '',
+
+        // 主题数
+        threads: '-',
+        // 回复数
+        posts: '-',
+        // 积分
+        credits: '-',
+
+        field4: '',
+        grouptitle: ''
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+        // 跳转到登录页面
+        toLogin() {
+            app.wxApi.navigateTo(loginPath)
+        },
+        // 跳转到设置页
+        toSetting() {
+            app.wxApi.navigateTo(settingPath)
+        },
+
+        // 获取用户信息（discuz）
+        requestProfile() {
+            let that = this
+            app.apimanager.getRequest(profileUrl)
+                .then(res => {
+                    let username = res.Variables.member_nickname ? res.Variables.member_nickname : res.Variables.member_username
+                    let avatar = res.Variables.member_avatar + "?t=" + Date.parse(new Date())
+
+                    if (res.Variables.auth) {
+                        let threads = res.Variables.space.threads
+                        let posts = res.Variables.space.posts
+                        let credits = res.Variables.space.credits
+                        let field4 = res.Variables.space.field4
+                        let grouptitle = res.Variables.space.group.grouptitle
+
+                        that.setData({
+                            username: username,
+                            avatar: avatar,
+                            threads: threads,
+                            posts: posts,
+                            credits: credits,
+                            field4: field4,
+                            grouptitle: grouptitle
+                        })
+
+                    } else {
+                        that.setData({
+                            username: username,
+                            avatar: avatar
+                        })
+                    }
+                })
+                .catch(e => {
+                    console.log(e)
+                    app.wxApi.showToast({ title: e.toString(), icon: 'none' })
+                })
+        }
+
     }
-  },
-
-  /**
-   * 组件所在页面的生命周期
-   */
-  pageLifetimes: {
-    show: function() {
-      // 页面被展示
-      let that = this
-      // 判断登录态
-      let uid = app.globalData.uid
-      let token = app.globalData.token
-      if (uid == '' || token == '') {
-        // 本地没有登录信息
-        that.setData({
-          avatar: userAvatar,
-          username: '',
-          uid: '',
-          threads: '-',
-          posts: '-',
-          credits: '-',
-          field4: '',
-          grouptitle: ''
-        })
-      } else {
-        // 本地存在登录信息
-        // 设置用户id
-        that.setData({
-          uid: uid
-        })
-        // 获取dz的用户信息
-        that.requestProfile()
-      }
-    },
-  },
-
-  /**
-   * 组件的初始数据
-   */
-  data: {
-    // 头像链接
-    avatar: userAvatar,
-    // 用户名（昵称）
-    username: '',
-    // 用户id
-    uid: '',
-
-    // 主题数
-    threads: '-',
-    // 回复数
-    posts: '-',
-    // 积分
-    credits: '-',
-
-    field4: '',
-    grouptitle: ''
-  },
-
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-    // 跳转到登录页面
-    toLogin() {
-      app.wxApi.navigateTo(loginPath)
-    },
-    // 跳转到设置页
-    toSetting(){
-      app.wxApi.navigateTo(settingPath)
-    },
-
-    // 获取用户信息（discuz）
-    requestProfile() {
-      let that = this
-      app.apimanager.getRequest(profileUrl)
-        .then(res => {
-          let username = res.Variables.member_nickname ? res.Variables.member_nickname : res.Variables.member_username
-          let avatar = res.Variables.member_avatar + "?t=" + Date.parse(new Date())
-
-          if (res.Variables.auth) {
-            let threads = res.Variables.space.threads
-            let posts = res.Variables.space.posts
-            let credits = res.Variables.space.credits
-            let field4 = res.Variables.space.field4
-            let grouptitle = res.Variables.space.group.grouptitle
-
-            that.setData({
-              username: username,
-              avatar: avatar,
-              threads: threads,
-              posts: posts,
-              credits: credits,
-              field4: field4,
-              grouptitle: grouptitle
-            })
-
-          } else {
-            that.setData({
-              username: username,
-              avatar: avatar
-            })
-          }
-        })
-        .catch(e => {
-          console.log(e)
-          app.wxApi.showToast({ title: e.toString(), icon: 'none' })
-        })
-    }
-
-  }
 })
